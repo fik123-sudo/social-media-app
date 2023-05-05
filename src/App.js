744e85a14b6d96bf7f66ab04e226d55748d14fa7
fikereyesus <fikereyesust@gmail.com>
2023-05-05 10:28:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import {Button} from '@mui/material';
+import {Add,Settings} from '@mui/icons-material';
+function App() {
+  return (
+    <div>
+     
+     
+      <Button variant="text">Text</Button>
+      <Button 
+      startIcon={<Settings/>} 
+      variant="contained" 
+      color = "secondary" 
+      size='small'>Contained
+      </Button>
+      <Button 
+      startIcon={<Add/>} 
+      variant="contained" 
+      color = "success" 
+      size='small'>Contained
+      </Button>
+      <Button variant="
+      outlined" disabled>Outlined</Button>     
+    </div>
+  );
+}
+
+export default App;
